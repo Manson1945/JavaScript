f3f29da5acc068217c6dd534424785815df39b2a
Sergio Ramirez <sergioramirez_89@hotmail.com>
2023-03-16 18:59:31 -0300

    2da Pre entrega

--- main.js
+++ main.js
@@ -1,142 +1,104 @@
 
-let nombre = prompt ("¡Hola, Bienvenido al Simulador!. \n \n Vamos a simular tu préstamo. Empecemos!! \n \n Ingresá tu nombre: ");
+class guitarra {
+    constructor (modelo, color, precio) {
+        this.modelo = modelo;
+        this.color = color;
+        this.precio = precio;
+        this.info =`Guitarra eléctrica ${this.modelo}  \n\nColor: ${this.color} \n\nSu precio promocional es de: $ ${this.precio}`;
+        }
 
+        verInfo() {
 
-let edad = parseInt ( prompt ("Hola " + nombre + "!. Ingresá tu edad: "));
+          console.log (this.info);
 
-
-
-while (edad < 18) {
-
-    
-    alert ("Sos menor de edad, no podés acceder al préstamo. Lo sentimos.");
-    
-    edad = parseInt ( prompt ("Debes ser mayor de edad. Ingresá tu edad: "));
-    
+    }  
 }
 
-
-while (edad >= 18) {
-    alert  ("Sos mayor de edad, podés acceder al préstamo. \n \n Gracias por elegirnos!. \n \n Aceptá para continuar.");
-    break;
-}
-
-
-let opcion1 = 1
-
-let opcion2 = 2
-
-
-let tipoPrestamo = parseInt (  prompt  ("A continuación elegí un tipo de Préstamo:  \n \n1- Préstamo personal  (De 3 a 60 Cuotas - T.N.A 83%) \n \n2- Préstamo personal UVA (De 3 a 24 Cuotas - T.N.A 36%) \n \n"));
-
-while ((tipoPrestamo < 1) || (tipoPrestamo > 2)) {
-   
-    alert ("La opción no es correcta. Vuelve a intentarlo");
-    
-    tipoPrestamo = parseInt ( prompt  ("A continuación elegí un tipo de Préstamo:  \n \n1- Préstamo personal  (De 3 a 60 Cuotas - T.N.A 83%) \n \n2- Préstamo personal UVA (De 3 a 24 Cuotas - T.N.A 36%) \n \n"))
-    
- if ((tipoPrestamo == opcion1) || (tipoPrestamo == opcion2)) {
-        break;
- }
-    
-
-   
-}
-
-
-
-
-let monto
-
-let cuotas
-
-function calculos (num1) {  
-    
-    let resultado =  ((( monto * num1 )/100) / cuotas)
-
-    alert ("Usted debería abonar " + cuotas + (" cuotas de $ ") + resultado) + ("!");
-    
-    
-}
-
-
-
-
- if (tipoPrestamo == opcion1) {
-
-  monto = parseFloat( prompt  ("                      ===== Elegiste Préstamo personal ===== \n \n Tu disponibilidad para simular el plan de pagos es de $2.500.000. \n \n  Elige un monto: "))
-    
-  cuotas = parseFloat( prompt (("Tu monto es de $" ) + monto + (". Ingresá la cantidad de Cuotas: \n \n (De 3 a 60 Cuotas) " )));
-
-  calculos (83);
-
-}
-
-
-
-
-
-else if  (tipoPrestamo == opcion2)  {
-
-    monto = parseFloat( prompt ("                    ===== Elegiste Préstamo personal UVA ===== \n Tu disponibilidad para simular el plan de pagos es de $800.000 \n Elige un monto: "));
-
-    cuotas = parseFloat( prompt (("Tu monto es de $") + monto + ("Ingresá la cantidad de Cuotas: \n \n (De 3 a 24 Cuotas) " )))
-
-    calculos (36)
-  
-} 
-
-
-
-else ((tipoPrestamo == opcion1) || (tipoPrestamo == opcion2)); {
-
-alert ("Gracias por haber utilizado los servicios de este simulador de préstamos!. \n \n Vuelva prontoss!.")
-   
-}
-   
    
 
 
+const guitarGibson = new guitarra ("Gibson Les Paul SG358", "Camel", "82485") ; 
+
+const guitarIbanez = new guitarra ("Ibanez RG350QA Gio Series", "Black", "108272");
+
+const guitarJackson = new guitarra ("Jackson Rhoads RR GBK", "Black & White", "160000");
+
+const guitarLesPaul = new guitarra ("Les Paul P253 Texas", "Sunburst", "75578");
+
+const guitarFender = new guitarra ("Fender Stratocaster T635 Classic", "White", "130521");
+
+const guitarCort = new guitarra ("Cort G100 HH Walnut Series", "Brown", "102512");
 
 
 
+const guitarrasArray = [guitarGibson, guitarIbanez, guitarJackson, guitarLesPaul, guitarFender, guitarCort];
+
+
+guitarrasArray[0].verInfo();
+guitarrasArray[1].verInfo();
+guitarrasArray[2].verInfo();
+guitarrasArray[3].verInfo();
+guitarrasArray[4].verInfo();
+guitarrasArray[5].verInfo();
+
+
+const ordenadas = guitarrasArray.sort ((a,b) => a.precio - b.precio);
+
+console.log(ordenadas);
 
 
 
+alert ("Bienvenido al simulador de compra Online!!. \n\nAceptá para continuar! ");
+
+let nombre = prompt ("Ingresá tu nombre: ");
+
+let valor = parseInt ( prompt (`${nombre} Podés filtrar las guitarras por su precio. Adelante: \n (Precios entre $75.000 y $160.000)`));
 
 
 
+const arrayFiltro= guitarrasArray.filter(guitarra => (guitarra.precio >= 75578) && (guitarra.precio <= 82484 )) ;
+
+const arrayFiltro2 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 102511 )) ;
+
+const arrayFiltro3 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 108271 )) ;
+
+const arrayFiltro4 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 130520 )) ;
+
+const arrayFiltro5 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 160000 )) ;
 
 
+while ((valor < 75578) || (valor > 160000)) { 
 
+    alert ("No hay ninguna guitarra de ese valor. Lo sentimos.");
 
+    valor = parseInt ( prompt ("Podes filtrar las guitarras por su precio. Adelante: \n (Precios entre $75.000 y $160.000)"));
 
+}
 
+if ((valor >= 75578) && (valor <= 82485)) {
 
+console.log(arrayFiltro);
 
+}
 
+else if ((valor >= 75578) && (valor <= 102512)) {
 
+ console.log(arrayFiltro2);
+}
 
+else if ((valor >= 75578) && (valor <= 108272 )) {
 
+    console.log(arrayFiltro3);
+}
 
+else if ((valor >= 75578) && (valor <= 130521  )) {
 
+    console.log(arrayFiltro4);
+}
 
+else if ((valor >= 75578) && (valor <= 160000  )) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log(arrayFiltro5);
+}
 
 
