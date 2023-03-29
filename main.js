fbe855bfdfaa228f27ac2e363ff703136f46065c
Sergio Ramirez <sergioramirez_89@hotmail.com>
2023-03-29 20:15:36 -0300

    3ra Pre Entrega

--- main.js
+++ main.js
@@ -1,104 +1,215 @@
 
-class guitarra {
-    constructor (modelo, color, precio) {
-        this.modelo = modelo;
-        this.color = color;
-        this.precio = precio;
-        this.info =`Guitarra eléctrica ${this.modelo}  \n\nColor: ${this.color} \n\nSu precio promocional es de: $ ${this.precio}`;
+const guitarras = [
+{
+    nombre: "guitarra1",
+
+    precio: 82485,
+
+    id: 1,
+
+    img: "./ASSETS/Guitarras/Guitarra Eléctrica Gibson Les Paul SG358 Camel $82.485.jpg",
+
+    info: "Gibson Les Paul SG358 Camel <br> <span>$82.485</span>"
+},
+
+{
+
+    nombre: "guitarra2",
+
+    precio: 108272,
+
+    id: 2,
+
+    img: "./ASSETS/Guitarras/Guitarra Eléctrica Ibanez RG350.jpg",
+
+    info: "Ibanez RG350QA Gio Series <br> <span>$108.272</span>"
+},
+
+{
+
+    nombre: "guitarra3",
+
+    precio: 160000 ,
+
+    id: 3,
+
+    img: "./ASSETS/Guitarras/Guitarra Eléctrica Jackson Rhoads RR GBK.jpg",
+
+    info: "Jackson Rhoads RR GBK <br> <span>$709.658</span>"
+},
+
+{
+
+    nombre: "guitarra4",
+
+    precio: 75578,
+
+    id: 4,
+
+    img: "./ASSETS/Guitarras/Guitarra Eléctrica Texas P253 Les Paul Sunburst Oscuo $76.578.jpg",
+
+    info: "Les Paul P253 Texas Sunburst <br> <span>$76.578</span>"
+},
+
+{
+
+    nombre: "guitarra5",
+
+    precio: 130521,
+
+    id: 5,
+
+    img: "./ASSETS/Guitarras/52236_Fender_Custom_Shop_Robin_Trower_Strat_R97633_2_9e91fbaa-0214-41e7-abda-719f78d86ba0_grande.webp",
+
+    info: "Fender Stratocaster T635 Classic <br> <span>$630.521</span>"
+},
+
+{
+
+    nombre: "guitarra6",
+
+    precio: 102512,
+
+    id: 6,
+
+    img: "./ASSETS/Guitarras/Cort G100 HH.jpg",
+
+    info: "Cort G100 HH Walnut Series <br> <span>$102.512</span>"
+},
+
+]
+
+const main = document.querySelector (".main-box");
+
+const carrito =JSON.parse(localStorage.getItem("carrito")) || [];
+
+const cart = document.querySelector (".carrito");
+
+
+mostrarCarrito();
+
+
+function crearMain() {
+
+guitarras.forEach ((guitars) => {
+ 
+    main.innerHTML +=  `    <div class= "divContainer">
+                            <div class= "picture">
+
+                            <div class="imgBox">
+                                <img src="${guitars.img}">    </img>
+
+                            </div>    
+
+                            <div class="info">
+                                ${guitars.info}
+
+                            </div>
+                            
+                            <div class= "buttons">
+
+                                <button class="sale" id="btnSale ${guitars.id}">COMPRAR</button>
+                                <button class="shop" id="btnShop ${guitars.id}">Agregar <i class="fa-sharp fa-solid fa-cart-shopping" ></i></button>
+
+                                </div>
+
+                            
+
+                     </div>
+                     </div>` 
+
+    
+
+})
+btnsFuncionalesSale()
+btnsFuncionalesShop()
+}
+
+function btnsFuncionalesSale () {
+
+    guitarras.forEach ((guitars) => {
+    
+         document.getElementById (`btnSale ${guitars.id}`).addEventListener ("click" , ()=>{
+            console.log(guitars);
+         }) 
+         
+    
+        
+    })
+    }
+
+function btnsFuncionalesShop () {
+
+    guitarras.forEach ((guitars) => {
+        
+            document.getElementById (`btnShop ${guitars.id}`).addEventListener ("click" , ()=>{
+            agregarAlCarrito(guitars)
+            }) 
+             
+        
+            
+    })
+    }
+
+
+
+
+function agregarAlCarrito (guitars) {
+
+    let aniadir = carrito.some((element) => element.id == guitars.id);
+
+        if (aniadir === false) {
+
+        guitars.cantidad = 1;
+
+        carrito.push (guitars)
         }
 
-        verInfo() {
+        else {
+            let myGuitar = carrito.find((element) => element.id == guitars.id)
 
-          console.log (this.info);
+            myGuitar.cantidad++;
+        }
+        localStorage.setItem("carrito" , JSON.stringify(carrito));
 
-    }  
+        mostrarCarrito ();
+
+    
 }
 
-   
 
 
-const guitarGibson = new guitarra ("Gibson Les Paul SG358", "Camel", "82485") ; 
+function mostrarCarrito () {
+        cart.innerHTML = "";
 
-const guitarIbanez = new guitarra ("Ibanez RG350QA Gio Series", "Black", "108272");
+    carrito.forEach ((guitars) => {
+        
+        cart.innerHTML +=      
+                               ` <div class="infoCarrito">
 
-const guitarJackson = new guitarra ("Jackson Rhoads RR GBK", "Black & White", "160000");
-
-const guitarLesPaul = new guitarra ("Les Paul P253 Texas", "Sunburst", "75578");
-
-const guitarFender = new guitarra ("Fender Stratocaster T635 Classic", "White", "130521");
-
-const guitarCort = new guitarra ("Cort G100 HH Walnut Series", "Brown", "102512");
-
-
-
-const guitarrasArray = [guitarGibson, guitarIbanez, guitarJackson, guitarLesPaul, guitarFender, guitarCort];
-
-
-guitarrasArray[0].verInfo();
-guitarrasArray[1].verInfo();
-guitarrasArray[2].verInfo();
-guitarrasArray[3].verInfo();
-guitarrasArray[4].verInfo();
-guitarrasArray[5].verInfo();
-
-
-const ordenadas = guitarrasArray.sort ((a,b) => a.precio - b.precio);
-
-console.log(ordenadas);
-
-
-
-alert ("Bienvenido al simulador de compra Online!!. \n\nAceptá para continuar! ");
-
-let nombre = prompt ("Ingresá tu nombre: ");
-
-let valor = parseInt ( prompt (`${nombre} Podés filtrar las guitarras por su precio. Adelante: \n (Precios entre $75.000 y $160.000)`));
-
-
-
-const arrayFiltro= guitarrasArray.filter(guitarra => (guitarra.precio >= 75578) && (guitarra.precio <= 82484 )) ;
-
-const arrayFiltro2 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 102511 )) ;
-
-const arrayFiltro3 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 108271 )) ;
-
-const arrayFiltro4 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 130520 )) ;
-
-const arrayFiltro5 = guitarrasArray.filter(guitarra => (guitarra.precio >= 75578 ) && (guitarra.precio <= 160000 )) ;
-
-
-while ((valor < 75578) || (valor > 160000)) { 
-
-    alert ("No hay ninguna guitarra de ese valor. Lo sentimos.");
-
-    valor = parseInt ( prompt ("Podes filtrar las guitarras por su precio. Adelante: \n (Precios entre $75.000 y $160.000)"));
+                                    <p class="cartText"> Agregaste al carrito ${guitars.info}</p>
+                                    
+                                    <p class="cartText"> Cantidad: ${guitars.cantidad}</p>
+                                    
+    
+                                </div> `
+      
+                                              
+})
 
 }
+                           
 
-if ((valor >= 75578) && (valor <= 82485)) {
 
-console.log(arrayFiltro);
+crearMain()
 
-}
 
-else if ((valor >= 75578) && (valor <= 102512)) {
 
- console.log(arrayFiltro2);
-}
 
-else if ((valor >= 75578) && (valor <= 108272 )) {
 
-    console.log(arrayFiltro3);
-}
 
-else if ((valor >= 75578) && (valor <= 130521  )) {
 
-    console.log(arrayFiltro4);
-}
 
-else if ((valor >= 75578) && (valor <= 160000  )) {
 
-    console.log(arrayFiltro5);
-}
 
 
