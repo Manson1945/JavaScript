bc085d8052fd199ee0074c187923d57aa12900ee
Sergio Ramirez <sergioramirez_89@hotmail.com>
2023-04-16 12:58:15 -0300

    Commit para pruebas

--- carrito.js
+++ carrito.js
@@ -1,85 +1,6 @@
 
 
-    let guitarras = []
-
-    fetch("./guitarras.json")
-        .then((response) => response.json())
-        .then((info) => {
-            guitarras = info;
-
-            guardarGuitarrasLS (guitarras); 
-             
-        })
-
-        .catch((error)=> {
-            console.error("error al cargar el archivo JSON:" , error)
-        });
-
-
-    /* GUARDAR Y CARGAR GUITARRAS EN LS */
-
-
-   
-    function guardarGuitarrasLS (guitarras) {
-
-        localStorage.setItem ("guitarras" , JSON.stringify(guitarras));
-
-        
-    }  
-
-
-    function cargarGuitarrasLS () {
-
-        return JSON.parse(localStorage.getItem("guitarras")) || [];
-    }
-
-
-    
-
-
-    /* RENDER DE MAIN */
-
-
-    function renderGuitarras () {
-
-        const guitarras = cargarGuitarrasLS();
-
-        let  send = "";
-    
-        for (producto of guitarras) {
-    
-            send +=  `<div class= "divContainer">
-                             <div class= "picture">
-    
-                             <div class="imgBox">
-                                 <img src="${producto.img}">    </img>
-                     
-                             </div>    
-    
-                             <div class="info">
-                                 ${producto.info} 
-    
-                             </div>
-            
-                             <div class= "buttons">
-    
-                                 <button class="sale" id="btnSale"><a href="${producto.page}">COMPRAR</a></button>
-                                 <button class="shop" id="btnShop" onclick= "agregarAlCarrito(${producto.id});">Agregar <i class="fa-sharp fa-solid fa-cart-shopping" ></i></button>
-    
-                            </div>
-                      </div>
-                    </div>` 
-        }
-
-        document.querySelector(".mainBox").innerHTML = send;
-
-        
-    }
-    
-     
-
-
-    /* CARRITO */
+/* CARRITO */
 
 
  function guardarProductosCarrito (guitarras) {
@@ -251,17 +172,7 @@ function carritoVacio () {
 }
 
 
-    cargarGuitarrasLS (guitarras);
-    renderGuitarras(); 
 
 
-    
-   
-
-
-
-
-
-
-
-
+renderProductosCarrito()
+renderCarrito()
